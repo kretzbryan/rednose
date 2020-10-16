90d33d0c332a1bb8e1205bfa096d7a03c5cb94d0
bryan kretz <kretzbryan@gmail.com>
2020-10-16 12:44:56 -0700

    user model edited

--- models/Gig.js
+++ models/Gig.js
@@ -4,6 +4,7 @@ const gigSchema = new mongoose.Schema({
     title: {type: String, required: true},
     location: {type: String, required: true},
     text: {type: String, required: true},
+    name: {type: String, require: true},
     author: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
