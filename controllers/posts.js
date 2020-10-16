2b5bb764f6630f75ddbc1f983febe52c41e2c1cd
bryan kretz <kretzbryan@gmail.com>
2020-10-15 22:44:24 -0700

    post controller overwritten

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const auth = require('../middleware/auth');
 
-router.get('/', (req, res) => {
-    db.Post.find({} ,(err, posts) => {
-        if (err) {
-            res.status(200).json({ message: 'Internal Server Error' })
-        } else {
-            res.status(200).json({ posts })
-        }
-    })
+
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await db.Post.find().sort({ 'createdAt': -1 });
+        res.json(posts)
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error')
+    }
 })
 
+router.put('/:id', auth, async (req, res) => {
+     try {
+        const post = await db.Post.findByIdAndUpdate(
+            req.params.id, 
+            req.body, 
+            {new:true}
+        )
+        await post.save();
+        console.log(post)
+        res.json(post)
+     } catch (err) {
+         res.json(err.message)
+     }
+})
 module.exports = router;
